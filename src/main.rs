b922bdaad870d31b9f526b411ce0b8a925cf7b7b
nickschmitt <nickschmitt@gmail.com>
2021-02-05 18:24:54 -0800

    add cat route in progress

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,53 @@
+#[macro_use]
+extern crate diesel;
+
 use actix_files::Files;
-use actix_web::{web, App, HttpServer, HttpResponse};
+use actix_web::{http, web, App, Error, HttpResponse, HttpServer};
+use std::env;
+use diesel::prelude::*;
+use diesel::pg::PgConnection;
+use diesel::r2d2::{self, ConnectionManager};
+use serde::Serialize;
+
+use self::models::*;
 
 use handlebars::Handlebars;
-use serde_json::json;
 
-async fn index(hb: web::Data<Handlebars<'_>>) -> HttpResponse { 
-    let data = json!({
-        "project_name": "Catdex",
-        "cats": [
-            {
-                "name":"Bengal",
-                "image_path":"/static/image/Bengal_Cat.jpg"
-            },
-            {
-                "name":"Persian",
-                "image_path":"/static/image/White_Persian_Cat.jpg"
-            },
-            {
-                "name":"Maine Coon",
-                "image_path":"/static/image/Maine_Coon_Cat.jpg"
-            }
-        ]
-    });
+mod models;
+mod schema;
+use self::schema::cats::dsl::*; // cats alias
 
-    let body = hb.render("index", &data).unwrap();
-    HttpResponse::Ok().body(body)
+// PgConnection comes from diesel::prelde
+type DbPool = r2d2::Pool<ConnectionManager<PgConnection>>;
+
+#[derive(Serialize)]
+struct IndexTemplateData {
+    project_name: String,
+    cats: Vec<self::models::Cat>
 }
+
+async fn index(hb: web::Data<Handlebars<'_>>, pool: web::Data<DbPool>) -> Result<HttpResponse, Error> { 
+        let connection = pool.get()
+            .expect("Can't get db connection from pool");
+        let cats_data = web::block(move || { cats.limit(100).load::<Cat>(&connection)
+        })
+        .await
+        .map_err(|_| HttpResponse::InternalServerError().finish())?;
+        let data = IndexTemplateData { 
+            project_name: "Catdex".to_string(), 
+            cats: cats_data,
+        };
+        let body = hb.render("index", &data).unwrap();
+        Ok(HttpResponse::Ok().body(body))
+}
+
+async fn add(hb: web::Data<Handlebars<'_>>) -> Result<HttpResponse, Error> {
+
+    let body = hb.render("add", &{}).unwrap();
+
+    Ok(HttpResponse::Ok().body(body))
+}
+
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> { 
@@ -35,15 +57,25 @@ async fn main() -> std::io::Result<()> {
         .unwrap();
     let handlebars_ref = web::Data::new(handlebars);
 
+    // setting up daabase conneciton pool
+    let database_url = env::var("DATABASE_URL")
+        .expect("DATABASE_URL must be set");
+    let manager = ConnectionManager::<PgConnection>::new(&database_url);
+    let pool = r2d2::Pool::builder()
+        .build(manager)
+        .expect("Failed to create DB connection pool.");
+
     println!("Listening on port 8080");
     HttpServer::new(move || {
         App::new()
             .app_data(handlebars_ref.clone())
+            .data(pool.clone())
             .service(
                 Files::new("/static", "static")
                     .show_files_listing(),
             )
             .route("/", web::get().to(index))
+            .route("/add", web::get().to(add))
     })
     .bind("127.0.0.1:8080")?
     .run()
